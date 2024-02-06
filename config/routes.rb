6921f29394e457d3753db7a5cf0c35ee55dc0a45
Maiwand Kakar <en.maiwand@gmail.com>
2024-02-06 17:14:24 +0300

    Routes had added to the project.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :pets do
+    resources :bookings
+    resources :records
+    resources :posts
+    resources :messages, only: [:show, :create]
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
