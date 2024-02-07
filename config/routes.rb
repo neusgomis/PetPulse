1c92347cb61fb150058a9df7a2756ac624543ec9
exec zsh <marianavaladares@gmail.com>
2024-02-07 15:27:59 +0000

    updated avatar pics

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :pets do
-    resources :bookings
-    resources :records
-    resources :posts
-    resources :messages, only: [:show, :create]
-  end
+        resources :pets do
+          resources :bookings
+          resources :records
+          resources :posts
+          resources :messages, only: [:show, :create]
+        end
 
-  get '/dashboard', to: 'pages#dashboard'
-end
+        get '/dashboard', to: 'pages#dashboard'
+      end
