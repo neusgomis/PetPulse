921b77329309f65759ee8ad26b5ea7dd0a8e6095
Maiwand Kakar <en.maiwand@gmail.com>
2024-02-07 12:40:07 +0300

    Form has added to the booking page.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,25 +4,30 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+    @pet = Pet.find(params[:pet_id])
+  end
+
   def new
     @pet = Pet.find(params[:pet_id])
     @booking = Booking.new
   end
 
-def create
-  @pet = Pet.find(params[:pet_id])
-  @booking = Booking.new(booking_params)
-  @booking.pet = @pet
+  def create
+    @pet = Pet.find(params[:pet_id])
+    @booking = Booking.new(booking_params)
+    @booking.pet = @pet
+    @booking.user_id = current_user
 
-
-  if @booking.save
-    redirect_to pet_bookings_path(@pet)
-  else
-    render :new, status: :unprocessable_entity
+    if @booking.save
+      redirect_to pets_path(@pet) # Modify this line to redirect to the desired page
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
   def booking_params
-    params.require(:booking).permit(:date_time, :description)
+    params.require(:booking).permit(:date_time, :description, :user_id, :pet_id)
   end
 end
