b979b10d2e78c206fb50c2976958726811679b6e
Oli Skertchly <olitherat@gmail.com>
2024-02-08 16:50:29 +0000

    moving cloudinary to aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.active_storage.service = :amazon
 
-  config.active_storage.service = :cloudinary
+  # config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
