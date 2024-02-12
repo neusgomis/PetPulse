b5f615bc2ce5047bfa238bf443083f7da49eac65
exec zsh <marianavaladares@gmail.com>
2024-02-12 11:48:16 +0000

    added recent events and rendered calendar to both pets dashboard and specific pet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,8 @@ class PagesController < ApplicationController
     @pet_new = Pet.new # for the form
     @created_pets = Pet.all
     @vets = User.where(vet: true)
+    @bookings = current_user.bookings
+    @booking = Booking.new
+    @bookings = Booking.where("pet_id = ? AND date_time >= ?", @pet.id, Time.now).order(:date_time).limit(6)
   end
 end
